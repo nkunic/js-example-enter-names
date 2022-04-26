00186f49a222673bd3547761bdb10c56e1201fe6
nkunic <natasa.kunic@gmail.com>
2022-04-26 19:20:22 +0200

    1 file modified

--- index.js
+++ index.js
@@ -2,3 +2,30 @@
 import './style.css';
 
 // Write Javascript code!
+
+/*** ENTER NAMES ***/
+var names = [];
+
+function askUser() {
+  //var nameValue = prompt('Enter name!');
+
+  if (nameValue !== 'kraj') {
+    names[names.length] = nameValue;
+    askUser();
+  } else {
+    displayNames();
+  }
+}
+askUser();
+
+//console.log(names);
+
+// for(var i=0; i < names.length; i++){
+//   console.log(names[i]);
+// }
+
+function displayNames() {
+  for (var i = 0; i < names.length; i++) {
+    console.log(names[i]);
+  }
+}
